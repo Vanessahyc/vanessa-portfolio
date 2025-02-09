0695c1e007609464a6b0779a815ffc0ea6a17140
Vanessahyc <hyc011224@gmail.com>
2025-02-09 12:39:12 -0800

    add fitquest project images and content

---
+++ .history/src/app/fitquest/page_20250207162423.js
@@ -0,0 +1,5 @@
+"use client";
+import Header from "../../../components/Header";
+import styles from "@/app/floom/Floom.module.css";
+import Image from "next/image";
+import { useState } from "react";
---
+++ .history/src/app/fitquest/page_20250207162455.js
@@ -0,0 +1,12 @@
+"use client";
+import Header from "../../../components/Header";
+import styles from "@/app/floom/Floom.module.css";
+import Image from "next/image";
+import { useState } from "react";
+
+export default function Fitquest() {
+    
+    return(
+
+    );
+}
