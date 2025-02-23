c1da4b5315a424d4405067fcd211a4d6d65cc964
Vanessahyc <hyc011224@gmail.com>
2025-02-23 14:32:05 -0800

    update: add links to projects, gallery, about and contact page. update padding style

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,15 +1,31 @@
 import Image from "next/image";
-import Link from 'next/link';
-import styles from './Header.module.css'
+import Link from "next/link";
+import styles from "./Header.module.css";
 
 export default function Header() {
   return (
     <nav className={`${styles.navbar}`}>
-      <Image src='/images/Logo.svg' alt="Vanessa Huang Logo" width={60} height={60}/>
+      <Link href="/">
+        <Image
+          src="/images/Logo.svg"
+          alt="Vanessa Huang Logo"
+          width={60}
+          height={60}
+        />
+      </Link>
       <ul>
-        <li><Link href="/">Home</Link></li>
-        <li><Link href="/about">About</Link></li>
-        <li><Link href="/contact">Contact</Link></li>
+        <li>
+          <Link href="/projects">Projects</Link>
+        </li>
+        <li>
+          <Link href="/gallery">Gallery</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/contact">Contact</Link>
+        </li>
       </ul>
     </nav>
   );
