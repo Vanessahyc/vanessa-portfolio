1e108dceb4a1d3d0f57030b321f87e6156446b6c
Vanessahyc <hyc011224@gmail.com>
2025-02-26 00:20:24 -0800

    add project filter and replace projectSmall component

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,94 +1,82 @@
+"use client";
 import Header from "../../../components/Header";
 import Footer from "../../../components/Footer";
-import Link from "next/link";
 import styles from "./Projects.module.css";
+import ProjectSmall from "../../../components/ProjectSmall";
+import { useState, useEffect } from "react";
 
 export default function Projects() {
+  const [projects, setProjects] = useState([]);
+  const [filteredProjects, setFilteredProjects] = useState([]);
+  const [selectedFilter, setSelectedFilter] = useState("All");
+
+  useEffect(() => {
+    fetch("/data/projects.json")
+      .then((res) => res.json())
+      .then((data) => {
+
+        const sortedProjects = data.sort((a, b) => {
+          if (b.year !== a.year) return b.year - a.year;
+          return b.month - a.month;
+        });
+
+        setProjects(sortedProjects);
+        setFilteredProjects(sortedProjects); 
+      })
+      .catch((error) => console.error("Error loading projects:", error));
+  }, []);
+
+
+  const handleFilterChange = (category) => {
+    setSelectedFilter(category);
+
+    if (category === "All") {
+      setFilteredProjects(projects);
+    } else {
+      setFilteredProjects(
+        projects.filter((project) => {
+          const projectCategories = Array.isArray(project.category)
+            ? project.category
+            : [project.category];
+          return projectCategories.includes(category);
+        })
+      );
+    }
+  };
+
   return (
     <>
       <Header />
       <main className={styles["main-container"]}>
         <section id="projects" className={styles["projects-section"]}>
           <h2 className={styles["section-title"]}>Recent Projects</h2>
+          <div className={styles["filter-container"]}>
+            {[
+              "All",
+              "Development",
+              "UI/UX Design",
+              "Graphic Design",
+              "Motion Graphics",
+            ].map((category) => (
+              <button
+                key={category}
+                className={`${styles["filter-button"]} ${
+                  selectedFilter === category ? styles["active"] : ""
+                }`}
+                onClick={() => handleFilterChange(category)}
+              >
+                {category}
+              </button>
+            ))}
+          </div>
           <div className={styles["projects-container"]}>
-            <Link href="/floom" className={styles["project-link"]}>
-              <div className={styles["project-box"]}>
-                <img
-                  src="/floom-images/floom_mockup.jpg"
-                  alt="floom packaging design"
-                />
-                <div className={styles["project-infobox"]}>
-                  <div className={styles["project-name"]}>
-                    <h3>Floom</h3>
-                    <p>2024</p>
-                  </div>
-                  <div className={styles["project-tagbox"]}>
-                    <p>Graphic Design</p>
-                    <p>Packaging Design</p>
-                  </div>
-                </div>
-              </div>
-            </Link>
-            {/* <Link href="/fitquest" className={styles["project-link"]}> */}
-            <div
-              className={`${styles["project-box"]} ${styles["box-overlay"]}`}
-            >
-              <img src="/images/fitquest.png" alt="FitQuest UI/UX Design" />
-              <div className={styles["project-infobox"]}>
-                <div className={styles["project-name"]}>
-                  <h3>FitQuest</h3>
-                  <p>2024</p>
-                </div>
-                <div className={styles["project-tagbox"]}>
-                  <p>UI/UX Design</p>
-                  <p>App Development</p>
-                </div>
-              </div>
-            </div>
-            {/* </Link> */}
-            <Link href="/relay" className={styles["project-link"]}>
-              <div className={styles["project-box"]}>
-                <img
-                  src="/relay-images/home_mockup@1.5x.png"
-                  alt="floom packaging design"
-                />
-                <div className={styles["project-infobox"]}>
-                  <div className={styles["project-name"]}>
-                    <h3>Relay</h3>
-                    <p>2024</p>
-                  </div>
-                  <div className={styles["project-tagbox"]}>
-                    <p>UI/UX Design</p>
-                    <p>App Development</p>
-                    <p>Case Study</p>
-                  </div>
-                </div>
-              </div>
-            </Link>
-            {/* <Link href="/fitquest" className={styles["project-link"]}> */}
-            <div
-              className={`${styles["project-box"]} ${styles["box-overlay"]}`}
-            >
-              <img
-                src="/ecospace-images/ecospace_mockup.jpg"
-                alt="EcoSpace Magazine Design"
-              />
-              <div className={styles["project-infobox"]}>
-                <div className={styles["project-name"]}>
-                  <h3>Eco Space</h3>
-                  <p>2024</p>
-                </div>
-                <div className={styles["project-tagbox"]}>
-                  <p>Content Design</p>
-                  <p>Graphic Design</p>
-                </div>
-              </div>
-            </div>
-            {/* </Link> */}
+            {filteredProjects.map((project) => (
+              <ProjectSmall key={project.id} {...project} />
+            ))}
           </div>
         </section>
       </main>
-      <Footer/>
+      <Footer />
     </>
   );
 }
