a6ed2dbc429ce00133877abdcd735d04ea8b398c
Vanessahyc <hyc011224@gmail.com>
2025-02-26 23:39:40 -0800

    update images for branding, style guide, marketing collateral and embed promotional video. Update content navigation link. Add back to top button

--- src/app/relay/page.js
+++ src/app/relay/page.js
@@ -1,77 +1,48 @@
 "use client";
+import { useState} from "react";
 import Header from "../../../components/Header";
 import styles from "@/app/relay/Relay.module.css";
 import Link from "next/link";
 import Image from "next/image";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, Autoplay } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
+import HeroSection from "../../../components/HeroSection";
+import Footer from "../../../components/Footer";
+import BackToTop from "../../../components/BackToTop"; 
 
 export default function Relay() {
+  const [modalImage, setModalImage] = useState(null);
+
+  const buttonData = [
+    {
+      href: "https://relay.arspera.com/",
+      text: "View Blog",
+      iconSrc: "/relay-images/btn_arrow.svg",
+    },
+  ];
+
+  const openImg = (src, alt) => {
+    setModalImage({ src, alt });
+  };
+
+  const closeImg = () => {
+    setModalImage(null);
+  };
+
   return (
     <>
       <Header />
       <main className={styles["main-container"]}>
-        <section className={styles["banner-section"]}>
-          <img
-            className={styles["banner-img"]}
-            src="/relay-images/home_mockup@1.5x.png"
-            alt="relay app homepage mockup"
-          />
-          <div className={styles["banner-info"]}>
-            <div className={styles["banner-header"]}>
-              <div className={styles["banner-title"]}>
-                <h2>Relay Ride-Sharing</h2>
-                <p>2024</p>
-              </div>
-              <p className={styles["banner-subtitle"]}>
-                App Development Project | Case Study
-              </p>
-            </div>
-
-            <div className={styles["banner-overview"]}>
-              <h3>Overview</h3>
-              <p>
-                Relay is Canada's first ridesharing app designed to assist busy
-                working parents in organizing safe and reliable transportation
-                for their children. The platform connects parents within a
-                trusted community to facilitate efficient and eco-friendly
-                ridesharing. The project aimed to address scheduling challenges
-                while ensuring security, convenience, and ease of use for
-                parents.
-              </p>
-              <div className={styles["btn-group"]}>
-                <Link
-                  className={styles["cta-btn"]}
-                  href="https://relay.arspera.com/"
-                  target="_blank"
-                >
-                  <img
-                    src="/relay-images/btn_arrow.svg"
-                    alt="arrow icon"
-                    className={styles["icon24"]}
-                  />
-                  <span>View Blog</span>
-                </Link>
-
-                <Link
-                  className={styles["cta-btn"]}
-                  href="https://www.figma.com/design/OPBMn5qhrbRMRWUSj1J58Q/Relay-Main-Screen?node-id=0-1&t=b9rDVCiFZs6dfTC7-1"
-                  target="_blank"
-                >
-                  <img
-                    src="/relay-images/btn_arrow.svg"
-                    alt="arrow icon"
-                    className={styles["icon24"]}
-                  />
-                  <span>View Prototype</span>
-                </Link>
-              </div>
-            </div>
-          </div>
-        </section>
+        <HeroSection
+          imageSrc="/relay-images/relay_hero_image.webp"
+          imageAlt="Relay Hero Image"
+          width={1500}
+          height={700}
+          title="Relay Ride-Sharing"
+          year="2024"
+          subtitle="App Development Project | Case Study"
+          overviewTitle="Overview"
+          overviewText="Relay is Canada's first ridesharing app designed to assist busy working parents in organizing safe and reliable transportation for their children. The platform connects parents within a trusted community to facilitate efficient and eco-friendly ridesharing. The project aimed to address scheduling challenges while ensuring security, convenience, and ease of use for parents."
+          buttons={buttonData}
+        />
 
         <section className={styles["section-container"]}>
           <div className={styles["section-title"]}>
@@ -89,54 +60,76 @@ export default function Relay() {
               <h4>Content</h4>
               <div className={styles["content-list"]}>
                 <ul>
-                  <li>User Research</li>
-                  <li>Problem | Solution</li>
-                  <li>User Persona</li>
+                  <Link href="#user-research">
+                    <li>User Research</li>
+                  </Link>
+                  <Link href="#user-persona">
+                    <li>User Persona</li>
+                  </Link>
                 </ul>
                 <ul>
-                  <li>App Workflow</li>
-                  <li>Wireframes</li>
-                  <li>Usability Test</li>
+                  <Link href="#branding">
+                    <li>Branding</li>
+                  </Link>
+                  <Link href="#styleguide">
+                    <li>Style Guide</li>
+                  </Link>
                 </ul>
                 <ul>
-                  <li>Branding</li>
-                  <li>Style Guides</li>
-                  <li>Marketing Collateral</li>
+                  <Link href="#app-workflow">
+                    <li>App Workflow</li>
+                  </Link>
+                  <Link href="#wireframes">
+                    <li>Wireframes</li>
+                  </Link>
+                </ul>
+                <ul>
+                  <Link href="#marketing-collateral">
+                    <li>Marketing Collateral</li>
+                  </Link>
+                  <Link href="#video">
+                    <li>Promotional Video</li>
+                  </Link>
                 </ul>
               </div>
             </div>
           </div>
         </section>
 
-        <section className={styles["section-container"]}>
+        <section id="user-research" className={styles["section-container"]}>
           <div className={styles["section-title"]}>
             <h3>User Research</h3>
           </div>
           <div className={styles["section-content"]}>
             <p>
-              User research uncovered key challenges parents face in managing
-              their children’s transportation. Three key insights emerged:
-              Scheduling conflicts — 75% of parents struggle to arrange
-              transportation due to conflicting schedules, and 67% report that
-              transportation issues have disrupted their children’s
-              participation in activities. Safety concerns —100% of parents
-              agree that GPS tracking is essential, and 83% prefer private
-              groups to organize carpools within trusted networks. Rideshare
-              demand — 75% of parents struggle to arrange transportation due to
-              conflicting schedules, and 67% report that transportation issues
-              have disrupted their children’s participation in activities. These
-              insights shaped a solution prioritizing safety,
-              convenience, and trusted connections.
+              Many working parents struggle to coordinate their children’s
+              transportation, often relying on last-minute solutions like asking
+              friends or family for help. Our research found that 75% of parents
+              face scheduling conflicts that make it difficult to arrange rides,
+              and 67% say transportation issues have caused their children to
+              miss activities. Safety is also a major concern, with all parents
+              agreeing that GPS tracking is essential, and 83% preferring
+              private groups to organize carpools with trusted people. Without a
+              reliable system in place, many families are left with limited
+              options, sometimes forcing children to wait, skip activities, or
+              take public transit alone. Despite the growing need for a safe and
+              convenient solution, no app in Canada currently provides an easy
+              way for parents to organize carpools within a trusted community.
+              These findings highlight the importance of creating a
+              transportation solution that prioritizes security, reliability,
+              and ease of use for families.
             </p>
             <Link
               className={styles["cta-btn"]}
               href="https://drive.google.com/file/d/1AFnJPVDrIHCMcRFgdjcG65a-SkyHVtI9/view"
               target="_blank"
             >
-              <img
+              <Image
                 src="/relay-images/btn_arrow.svg"
                 alt="arrow icon"
                 className={styles["icon24"]}
+                width={24}
+                height={24}
               />
               <span>User Finding Report</span>
             </Link>
@@ -168,7 +161,7 @@ export default function Relay() {
           </div>
         </section>
 
-        <section className={styles["section-container"]}>
+        <section id="user-persona" className={styles["section-container"]}>
           <div className={styles["section-title"]}>
             <h3>User Persona</h3>
           </div>
@@ -179,75 +172,54 @@ export default function Relay() {
             </p>
             <div className={styles["d-flex"]}>
               <div className={styles["width50"]}>
-                {/* <p>
-                    A full-time software engineer and father of a 10-year-old
-                    son, struggles to arrange reliable rides due to his
-                    demanding schedule. He values a tech-driven ridesharing
-                    solution with GPS tracking and reliable network.
-                  </p> */}
-                <img
+                <Image
                   className={styles.personaImg}
                   src="/relay-images/primary_persona.png"
-                  alt="placeholder image"
+                  alt="Primary persona"
+                  width={2338}
+                  height={1650}
+                  onClick={() =>
+                    openImg(
+                      "/relay-images/primary_persona.png",
+                      "Primary persona"
+                    )
+                  }
+                  style={{ cursor: "pointer" }}
                 />
               </div>
               <div className={styles["width50"]}>
-                {/* <p>
-                    A part-time climate policy advisor, enjoys helping her
-                    community by giving kids rides but finds it hard to
-                    coordinate with busy parents. She seeks a platform that
-                    simplifies scheduling, enhances communication, and builds
-                    connection.
-                  </p> */}
-                <img
+                <Image
                   className={styles.personaImg}
                   src="/relay-images/secondary_persona.png"
-                  alt="placeholder image"
+                  alt="Secondary persona"
+                  width={2338}
+                  height={1650}
+                  onClick={() =>
+                    openImg(
+                      "/relay-images/secondary_persona.png",
+                      "Secondary persona"
+                    )
+                  }
+                  style={{ cursor: "pointer" }}
                 />
               </div>
             </div>
           </div>
         </section>
 
-        <section className={styles["section-container"]}>
-          <div className={styles["section-title"]}>
-            <h3>App Workflow</h3>
+        {modalImage && (
+          <div className={styles.modal} onClick={closeImg}>
+            <Image
+              src={modalImage.src}
+              alt={modalImage.alt}
+              width={2338}
+              height={1650}
+              style={{ objectFit: "contain" }}
+            />
           </div>
-          <div className={styles["section-content"]}>
-            <p>
-              In response to the needs of busy parent to schedule carpools for
-              their kids. The design priority are: Home, Create carpool, Trips
-              and Community pages. Tutorials, Landing page, Message and Setting
-              pages are the secondary concerns.
-            </p>
-            <iframe
-              className={styles.figma}
-              src="https://embed.figma.com/board/UQ8HVMw4achSHFZ8b8NcGr/Relay---App-Workflow?node-id=284-1439&embed-host=share"
-              allowFullScreen
-            ></iframe>
-          </div>
-        </section>
+        )}
 
-        <section className={styles["section-container"]}>
-          <div className={styles["section-title"]}>
-            <h3>Wireframes</h3>
-          </div>
-          <div className={styles["section-content"]}>
-            <p>
-              In response to the needs of busy parent to schedule carpools for
-              their kids. The design priority are: Home, Create carpool, Trips
-              and Community pages. Tutorials, Landing page, Message and Setting
-              pages are the secondary concerns.
-            </p>
-            <iframe
-              className={styles.figma}
-              src="https://embed.figma.com/design/OPBMn5qhrbRMRWUSj1J58Q/Relay-Main-Screen?node-id=0-1&embed-host=share"
-              allowFullScreen
-            ></iframe>
-          </div>
-        </section>
-
-        <section className={styles["section-container"]}>
+        <section id="branding" className={styles["section-container"]}>
           <div className={styles["section-title"]}>
             <h3>Branding</h3>
           </div>
@@ -267,7 +239,7 @@ export default function Relay() {
           </div>
         </section>
 
-        <section className={styles["section-container"]}>
+        <section id="styleguide" className={styles["section-container"]}>
           <div className={styles["section-title"]}>
             <h3>Style Guide</h3>
           </div>
@@ -284,14 +256,139 @@ export default function Relay() {
               modern typeface, enhances warmth, trust, and readability, aligning
               with the community-focused design.
             </p>
-            <img
+            <Image
               className={styles.styleGuideImg}
-              src="/relay-images/relay_branding.png"
-              alt="placeholder image"
+              src="/relay-images/relay_styleguide.png"
+              alt="Relay Style Guide"
+              width={1704}
+              height={864}
             />
           </div>
         </section>
+
+        <section id="app-workflow" className={styles["section-container"]}>
+          <div className={styles["section-title"]}>
+            <h3>App Workflow</h3>
+          </div>
+          <div className={styles["section-content"]}>
+            <p>
+              In response to the needs of busy parent to schedule carpools for
+              their kids. The design priority are: Home, Create carpool, Trips
+              and Community pages. Tutorials, Landing page, Message and Setting
+              pages are the secondary concerns.
+            </p>
+            <iframe
+              className={styles.figma}
+              src="https://embed.figma.com/board/UQ8HVMw4achSHFZ8b8NcGr/Relay---App-Workflow?node-id=284-1439&embed-host=share"
+              allowFullScreen
+            ></iframe>
+          </div>
+        </section>
+
+        <section id="wireframes" className={styles["section-container"]}>
+          <div className={styles["section-title"]}>
+            <h3>Wireframes</h3>
+          </div>
+          <div className={styles["section-content"]}>
+            <p>
+              In response to the needs of busy parent to schedule carpools for
+              their kids. The design priority are: Home, Create carpool, Trips
+              and Community pages. Tutorials, Landing page, Message and Setting
+              pages are the secondary concerns.
+            </p>
+            <iframe
+              className={styles.figma}
+              src="https://embed.figma.com/design/EEfmHSXnxCvv1vu5rzZjrH/Relay---HiFI-Wireframes?node-id=0-1&embed-host=share"
+              allowFullScreen
+            ></iframe>
+          </div>
+        </section>
+
+        <section
+          id="marketing-collateral"
+          className={styles["section-container"]}
+        >
+          <div className={styles["section-title"]}>
+            <h3>Marketing Collateral</h3>
+          </div>
+          <div className={styles["section-content"]}>
+            <div className={styles["d-flex"]}>
+              <div className={styles["width50"]}>
+                <Image
+                  className={styles.personaImg}
+                  src="/relay-images/bc_front_mockup.webp"
+                  alt="Business Card Front"
+                  width={2000}
+                  height={1414}
+                  onClick={() =>
+                    openImg(
+                      "/relay-images/bc_front_mockup.webp",
+                      "Business Card Front View"
+                    )
+                  }
+                  style={{ cursor: "pointer" }}
+                />
+              </div>
+              <div className={styles["width50"]}>
+                <Image
+                  className={styles.personaImg}
+                  src="/relay-images/bc_angle_mockup.webp"
+                  alt="Business Card Angle View"
+                  width={2000}
+                  height={1333}
+                  onClick={() =>
+                    openImg(
+                      "/relay-images/bc_angle_mockup.webp",
+                      "Business Card Angle View"
+                    )
+                  }
+                  style={{ cursor: "pointer" }}
+                />
+              </div>
+            </div>
+            <Image
+              className={styles.styleGuideImg}
+              src="/relay-images/brochure_mockup.webp"
+              alt="Brochure Mockup"
+              width={2000}
+              height={1143}
+              onClick={() =>
+                openImg("/relay-images/brochure_mockup.webp", "Brochure Mockup")
+              }
+              style={{ cursor: "pointer" }}
+            />
+          </div>
+        </section>
+        {modalImage && (
+          <div className={styles.modal} onClick={closeImg}>
+            <Image
+              src={modalImage.src}
+              alt={modalImage.alt}
+              width={2338}
+              height={1650}
+              style={{ objectFit: "contain" }}
+            />
+          </div>
+        )}
+
+        <section id="video" className={styles["section-container"]}>
+          <div className={styles["section-title"]}>
+            <h3>Promotional Video</h3>
+          </div>
+          <div className={styles["section-content"]}>
+            <iframe
+              className={styles.figma}
+              src="https://www.youtube.com/embed/iqs0Pss_1fc?si=EPphv7Y1iCxDHmS_"
+              title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+            ></iframe>
+          </div>
+        </section>
       </main>
+      <BackToTop />
+      <Footer />
     </>
   );
 }
