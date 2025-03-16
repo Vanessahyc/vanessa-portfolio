79bde88ea815f34cc9176b3e8bf1f3dbc2fa89a9
Vanessahyc <hyc011224@gmail.com>
2025-03-15 22:50:42 -0700

    update gallery page

--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -1,33 +1,116 @@
-import styles from "./Gallery.module.css";
-import Header from "../../../components/Header";
-import Footer from "../../../components/Footer";
-import Link from "next/link";
+"use client";
 
-export default function Gallery() {
+import React, { useEffect } from "react";
+import LocomotiveScroll from "locomotive-scroll";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import Flip from "gsap/Flip";
+import CustomEase from "gsap/CustomEase";
+import Header from "../../../components/Header";
+import styles from "./Gallery.module.css";
+
+export default function GalleryPage() {
+  useEffect(() => {
+
+    gsap.registerPlugin(ScrollTrigger, Flip, CustomEase);
+    CustomEase.create("cubic", "0.83, 0, 0.17, 1");
+
+    const scroller = new LocomotiveScroll({
+      el: document.querySelector("[data-scroll-container]"),
+      smooth: true,
+    });
+
+    const gallery = document.querySelector(`.${styles.imgGalleryContainer}`);
+    const images = gsap.utils.toArray(`.${styles.img}`);
+    const btn = document.querySelector(`.${styles.btn}`);
+
+    let rotationValues = [10, -5, 2, -2];
+    let isFlipped = false;
+
+    function applyRotation() {
+      images.forEach((img, index) => {
+        const rotation =
+          isFlipped ? 0 : rotationValues[index % rotationValues.length];
+        gsap.to(img, {
+          rotate: rotation,
+          duration: 2,
+          ease: "cubic",
+          delay: 0.15,
+        });
+      });
+    }
+
+    btn.addEventListener("click", () => {
+      isFlipped = !isFlipped;
+
+      setTimeout(() => {
+        btn.textContent = isFlipped ? "Hide All Arts" : "Explore Gallery";
+      }, 1000);
+
+      let state = Flip.getState(
+        `.${styles.imgGalleryContainer}, .${styles.img}`
+      );
+      gallery.classList.toggle(styles.order);
+      images.forEach((img) => img.classList.toggle(styles.reorder));
+
+      Flip.from(state, {
+        absolute: true,
+        duration: 2,
+        rotate: 0,
+        stagger: 0.05,
+        ease: "cubic",
+        onStart: () => {
+          applyRotation();
+        },
+        onComplete: () => {
+          scroller.update();
+        },
+      });
+    });
+  }, []);
+
   return (
     <>
-      <Header />
-      <main>
-        <div className={styles["main-container"]}>
-          <section className={styles["greeting-section"]}>
-            <h1 className={styles["greeting-big"]}>
-              This page is under construction
-            </h1>
-            <h3 className={styles["greeting-small"]}>
-              Please check out other pages
-            </h3>
-            <Link className={styles["greeting-bottom"]} href="/projects">
-              <img
-                src="/relay-images/btn_arrow.svg"
-                alt="arrow icon"
-                className={styles["icon24"]}
-              />
-              <span>Checkout Projects</span>
-            </Link>
-          </section>
+    <Header className={styles.nav}/>
+    <div className={styles.galleryPage}>
+      <div className={styles.imgGallery} data-scroll-container>
+        <div
+          className={`${styles.imgGalleryContainer} ${styles.order}`}
+          data-scroll
+        >
+          <div className={`${styles.img} ${styles.reorder}`} id="i-1">
+            <img src="/test-images/img1.webp" alt="img1" />
+          </div>
+          <div className={`${styles.img} ${styles.reorder}`}>
+            <img src="/test-images/img2.webp" alt="img2" />
+          </div>
+          <div className={`${styles.img} ${styles.reorder}`}>
+            <img src="/test-images/img3.webp" alt="img3" />
+          </div>
+          <div className={`${styles.img} ${styles.reorder}`}>
+            <img src="/test-images/img4.webp" alt="img4" />
+          </div>
+          <div className={`${styles.img} ${styles.reorder}`}>
+            <img src="/test-images/img5.webp" alt="img5" />
+          </div>
+          <div className={`${styles.img} ${styles.reorder}`}>
+            <img src="/test-images/img6.webp" alt="img6" />
+          </div>
+          <div className={`${styles.img} ${styles.reorder}`}>
+            <img src="/test-images/img7.webp" alt="img7" />
+          </div>
+          <div className={`${styles.img} ${styles.reorder}`}>
+            <img src="/test-images/img8.webp" alt="img8" />
+          </div>
         </div>
-      </main>
-      <Footer />
+      </div>
+      <div className={styles.hero}>
+        <h1>Art Gallery</h1>
+      </div>
+      <div className={styles.btn}>Explore Gallery</div>
+
+    </div>
     </>
+    
   );
 }
